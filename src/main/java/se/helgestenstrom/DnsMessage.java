4aa4fd7a1e9566144e675fe79c059b8e72824fce
Helge Stenström <h.stenstrom@gmail.com>
2023-08-16 23:38:00 +0200

    First passing test
    trivially hardcoded.

---
+++ src/main/java/se/helgestenstrom/DnsMessage.java
@@ -0,0 +1,9 @@
+package se.helgestenstrom;
+
+public class DnsMessage {
+
+
+    public String message() {
+        return "00160100000100000000000003646e7306676f6f676c6503636f6d0000010001";
+    }
+}
--- src/test/java/DnsMessageTest.java
+++ src/test/java/DnsMessageTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import se.helgestenstrom.DnsMessage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,4 +10,18 @@ class DnsMessageTest {
         assertEquals(3, 1+2);
     }
 
+
+    @Test
+    void sampleMessageString() {
+
+        // Setup
+        var dnsMessage = new DnsMessage();
+
+        // Exercise
+        var message = dnsMessage.message();
+
+        // Verify
+        String messageExampleFromExercise = "00160100000100000000000003646e7306676f6f676c6503636f6d0000010001";
+        assertEquals(messageExampleFromExercise, message);
+    }
 }
