5185a30f1efe184ca64a167be00b8f269e9e0bfb
Helge Stenström <h.stenstrom@gmail.com>
2023-08-24 23:43:47 +0200

    Simplified ResourceRecordTest.foo, extract 3 names.

--- src/test/java/se/helgestenstrom/ResourceRecordTest.java
+++ src/test/java/se/helgestenstrom/ResourceRecordTest.java
@@ -18,12 +18,12 @@ class ResourceRecordTest {
     void foo() {
 
         String hexOfAnswers = "c00c0001000100000214000408080808c00c0001000100000214000408080404";
-        String exampleMessage = "00168080000100020000000003646e7306676f6f676c6503636f6d0000010001c00c0001000100000214000408080808c00c0001000100000214000408080404";
+        String exampleMessageHex = "00168080000100020000000003646e7306676f6f676c6503636f6d0000010001c00c0001000100000214000408080808c00c0001000100000214000408080404";
 
 
         /*
          * c00c.0001000100000214000408080808c00c0001000100000214000408080404
-         * c00c - 1100 0000 0000 1100 - offset 12 from start of exampleMessage
+         * c00c - 1100 0000 0000 1100 - offset 12 from start of exampleMessageHex
          * 0001 -
          * 0001 -
          * 0000 -
@@ -31,24 +31,24 @@ class ResourceRecordTest {
          * 1400 - offset a lot
          * */
 
-        String pointerHex = hexOfAnswers.substring(0, 4);
-
-        String partOfExampeMessage = exampleMessage.substring(64);
-        assertEquals(partOfExampeMessage, hexOfAnswers);
-        ByteList integers = ByteList.of(exampleMessage);
-
-        String readableMessage = getString(integers);
 
 
-        ResourceRecord resourceRecord = ResourceRecord.of(exampleMessage, 64);
-        var pointerBytes = integers.subList(32, 34);
+        ByteList wholeMessage = ByteList.of(exampleMessageHex);
+
+        String readableMessage = getString(wholeMessage);
+
+
+        ResourceRecord resourceRecord = ResourceRecord.of(exampleMessageHex, 64);
+        var pointerBytes = wholeMessage.subList(32, 34);
         int offsetWord = (pointerBytes.get(0) << 8) | pointerBytes.get(1);
         int mask = 0x3fff;
         int offset = offsetWord & mask;
 
-        var integers1 = integers.subList(offset, integers.size());
-        String string = getString(integers1);
-        DomainName domainName = DomainName.of(integers1, 0);
+        //var integers1 = wholeMessage.subList(offset, wholeMessage.size());
+        //String string = getString(integers1);
+        DomainName domainName1 = DomainName.of(wholeMessage, 12);
+        DomainName domainName2 = DomainName.of(wholeMessage, 32);
+        DomainName domainName3 = DomainName.of(wholeMessage, 48);
 
         fail("test not done");
         assertEquals(2, 1 + 3, "Test not done");
